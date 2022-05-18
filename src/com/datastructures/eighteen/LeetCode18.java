13e1ec9dd278bc7a6f2c2fc1c4a86c63426ddf1f
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-05-18 19:19:31 +0530

    DA-21 Problem 5

--- src/com/datastructures/eighteen/LeetCode18.java
+++ src/com/datastructures/eighteen/LeetCode18.java
@@ -1,5 +1,7 @@
 package com.datastructures.eighteen;
 
+import java.util.Arrays;
+
 /**
  * @author <a>Pulkit Aggarwal</a>
  * @version 1.0
@@ -7,10 +9,28 @@ package com.datastructures.eighteen;
  */
 public class LeetCode18 {
 	public static void main(String[] args) {
-
+		int[] b = new int[] {1,2 };
+	//	majorityElement(b);
 	}
 
+	// Q169
 	public static void majorityElement(int[] nums) {
 
+//		int value = 0;
+//		int count  = 0;
+//
+//		for(int num : nums){
+//			if(count == 0){
+//				value = num;
+//			}if(num == value){
+//				count++;
+//			}else {
+//				count--;
+//			}
+//		}
+
+		Arrays.sort(nums);
+
+		System.out.println(nums[nums.length/2]);
 	}
 }
