2ffc53dc90cb1eb6beea6642b63d18c3e9f14bc5
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-05-26 10:52:47 +0530

    DA-45 leetcode

--- src/com/datastructures/may/LeetCode25.java
+++ src/com/datastructures/may/LeetCode25.java
@@ -1,9 +1,6 @@
 package com.datastructures.may;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 /**
  * @author <a>Pulkit Aggarwal</a>
@@ -46,27 +43,26 @@ public class LeetCode25 {
 
 	public static boolean isSubsequence(String s, String t) {
 
-//		char[] sArray = s.toCharArray();
-//		char[] tArray = t.toCharArray();
-//
-//		Arrays.sort(sArray);
-//		Arrays.sort(tArray);
-//
-//		for (int i = 0; i < sArray.length; i++) {
-//			if (tArray[i] != sArray[i]) {
-//				System.out.println("false");
-//				return false;
-//			}
-//		}
-//
-//		System.out.println("true");
-//		return true;
-
+		//		char[] sArray = s.toCharArray();
+		//		char[] tArray = t.toCharArray();
+		//
+		//		Arrays.sort(sArray);
+		//		Arrays.sort(tArray);
+		//
+		//		for (int i = 0; i < sArray.length; i++) {
+		//			if (tArray[i] != sArray[i]) {
+		//				System.out.println("false");
+		//				return false;
+		//			}
+		//		}
+		//
+		//		System.out.println("true");
+		//		return true;
 
 		int sPtr = 0;
 		int tPtr = 0;
 
-		while (sPtr<s.length() && tPtr<t.length()) {
+		while (sPtr < s.length() && tPtr < t.length()) {
 			if (s.charAt(sPtr) == t.charAt(tPtr)) {
 				sPtr++;
 			}
@@ -84,14 +80,14 @@ public class LeetCode25 {
 
 		int[] res = new int[2];
 
-		while(firstPtr< numbers.length && secondPtr < numbers.length){
-			if(numbers[firstPtr] + numbers[secondPtr] > target ){
+		while (firstPtr < numbers.length && secondPtr < numbers.length) {
+			if (numbers[firstPtr] + numbers[secondPtr] > target) {
 				secondPtr--;
-			}else if(numbers[firstPtr] + numbers[secondPtr] < target){
+			} else if (numbers[firstPtr] + numbers[secondPtr] < target) {
 				firstPtr++;
-			}else {
+			} else {
 				res[0] = firstPtr + 1;
-				res[1] = secondPtr  + 1;
+				res[1] = secondPtr + 1;
 				break;
 			}
 		}
@@ -101,18 +97,117 @@ public class LeetCode25 {
 
 	public static boolean isPalindrome(String s) {
 
-		String newStr = s.replaceAll(" ","");
+		String newStr = s.replaceAll(" ", "");
 
 		StringBuilder stringBuilder = new StringBuilder();
 
 		String reverse = String.valueOf(stringBuilder.append(newStr).reverse());
 
-		if(newStr.equalsIgnoreCase(reverse)){
+		if (newStr.equalsIgnoreCase(reverse)) {
 			return true;
-		};
+		}
+		;
 
 		return false;
 	}
+
+	public static int[] reverse(int[] a) {
+		int n = a.length;
+		int t;
+		for (int i = 0; i < n / 2; i++) {
+			t = a[i];
+			a[i] = a[n - i - 1];
+			a[n - i - 1] = t;
+		}
+
+		return a;
+
+	}
+
+	public static int[] fn(int[] nums, int k) {
+
+		int[] a = Arrays.copyOfRange(nums, 0, k + 1);
+
+		int[] b = Arrays.copyOfRange(nums, k + 1, nums.length);
+
+		int[] result = new int[a.length + b.length];
+
+		System.arraycopy(b, 0, result, 0, b.length);
+		System.arraycopy(a, 0, result, b.length, a.length);
+
+		return result;
+	}
+
+	public static void rotate(int[] nums, int k) {
+
+		if (nums.length == 0) {
+			return;
+		}
+
+		nums = fn(nums, k);
+
+		for (int i = 0; i < nums.length; i++) {
+			int num = nums[i];
+		}
+
+		System.out.println(nums);
+
+	}
+
+	public static void reverseString(char[] s) {
+
+		char[] s1 = new char[s.length];
+
+		int j = 0;
+
+		for (int i = s1.length - 1; i >= 0; i--) {
+			s1[j] = s[i];
+			j++;
+		}
+
+		System.out.println(Arrays.toString(s1));
+	}
+
+	public static void reverseStr(String s, int k) {
+		String n = s.substring(0, k);
+		String j = s.substring(k);
+
+		System.out.println(n);
+		System.out.println(j);
+
+		StringBuilder stringBuilder = new StringBuilder();
+		n = stringBuilder.append(n).reverse().toString();
+
+		System.out.println(n);
+
+		String res = n.concat(j);
+		System.out.println(res);
+	}
+
+	public static double findMaxAverage(int[] nums, int k) {
+		double max_av = 0;
+
+		int max_sum = 0;
+
+		for (int i = 0; i < k; i++) {
+			max_sum += nums[i];
+		}
+
+		int window_sum = max_sum;
+
+		for(int i=k ; i < nums.length; i++){
+			window_sum += nums[i] - nums[i-k];
+
+			max_sum = Math.max(max_sum, window_sum);
+		}
+
+		System.out.println(max_sum);
+
+		max_av = (double) max_sum / k;
+		return max_av;
+	}
+
+
 
 	public static void main(String[] args) {
 		//System.out.println(lengthOfLastWord("\"   fly me   to   the moon  "));
@@ -121,10 +216,21 @@ public class LeetCode25 {
 
 		//isSubsequence("dd", "");
 
-		int[] a = new int[]{
-				2,7,11,15
+		int[] a = new int[] {
+				1,12,-5,-6,50,3
 		};
 		//System.out.println(Arrays.toString(twoSum(a, 9)));
-		isPalindrome("race a car");
+		//isPalindrome("race a car");
+
+		//rotate(a, 3);
+
+		char[] c = new char[] {
+				'h', 'e', 'l', 'l', 'o'
+		};
+		//reverseString(c);
+
+		//reverseStr("abcdefg", 2);
+
+		System.out.println(findMaxAverage(a,4));
 	}
 }
---
+++ src/com/datastructures/may/LinkedListLeetCode.java
@@ -0,0 +1,34 @@
+package com.datastructures.may;
+
+import java.util.LinkedList;
+
+/**
+ * @author <a>Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 25/05/22
+ */
+public class LinkedListLeetCode {
+
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static void main(String[] args) {
+		ListNode listNode = new ListNode();
+
+		listNode.val = 1;
+	}
+}
