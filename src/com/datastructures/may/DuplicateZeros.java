79f4fd7305893706866d08e7261a202f5a23b6dd
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-05-26 20:32:50 +0530

    DA-45 leetcode

---
+++ src/com/datastructures/may/DuplicateZeros.java
@@ -0,0 +1,34 @@
+package com.datastructures.may;
+
+import java.util.Arrays;
+
+/**
+ * @author <a>Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 26/05/22
+ */
+public class DuplicateZeros {
+	public static void duplicateZeros(int[] arr) {
+		int[] newArr =new int[arr.length];
+		int j = 0;
+		for(int i=0; i< arr.length;i++){
+			if(j == arr.length){
+				break;
+			}
+			newArr[j] = arr[i];
+			if(arr[i] == 0){
+				newArr[j+1] = 0;
+				j++;
+			}
+			j++;
+		}
+
+		System.out.println(Arrays.toString(newArr));
+	}
+
+	public static void main(String[] args) {
+		int[] a = new int[]{1,0,2,3,0,4,5,0};
+
+		duplicateZeros(a);
+	}
+}
