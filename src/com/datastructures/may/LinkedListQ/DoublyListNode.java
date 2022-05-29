b8d166d0ad0f53f7d0b4a81f3186051eb928c2ee
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-05-29 17:42:10 +0530

    DA-43 Linked List

---
+++ src/com/datastructures/may/LinkedListQ/DoublyListNode.java
@@ -0,0 +1,47 @@
+package com.datastructures.may.LinkedListQ;
+
+/**
+ * @author <a>Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 29/05/22
+ */
+public class DoublyListNode {
+	private int val;
+	private DoublyListNode prev;
+	private DoublyListNode next;
+
+	public DoublyListNode(int val) {
+		this.val = val;
+		this.next = null;
+	}
+
+	public DoublyListNode(int val, DoublyListNode prev, DoublyListNode next) {
+		this.val = val;
+		this.prev = prev;
+		this.next = next;
+	}
+
+	public int getVal() {
+		return val;
+	}
+
+	public DoublyListNode getPrev() {
+		return prev;
+	}
+
+	public DoublyListNode getNext() {
+		return next;
+	}
+
+	public void setVal(int val) {
+		this.val = val;
+	}
+
+	public void setPrev(DoublyListNode prev) {
+		this.prev = prev;
+	}
+
+	public void setNext(DoublyListNode next) {
+		this.next = next;
+	}
+}
