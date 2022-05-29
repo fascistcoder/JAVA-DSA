599ada75c6c00e643f439c9b29fb87f4a48eef82
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-05-29 18:16:16 +0530

    DA-43 Linked List

---
+++ src/com/datastructures/may/LinkedListQ/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+package com.datastructures.may.LinkedListQ;
+
+/**
+ * @author <a>Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 29/05/22
+ */
+public class ReverseLinkedList {
+
+	public MiddleNode.ListNode  reverseList(MiddleNode.ListNode head) {
+
+		if(head==null){
+			return null;
+		}
+
+		if(head.next==null){
+			return head;
+		}
+
+		MiddleNode.ListNode prev = null;
+		MiddleNode.ListNode next = null;
+		MiddleNode.ListNode currentNode = head;
+
+		while (currentNode != null){
+			next = currentNode.next;
+			currentNode.next = prev;
+			prev = currentNode;
+			currentNode = next;
+		}
+
+		return prev;
+
+	}
+}
