6c7871d81cc2e94d697455f77cd3731c567c829c
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-05-31 10:13:39 +0530

    binary search

---
+++ src/com/datastructures/may/BinarySearch.java
@@ -0,0 +1,27 @@
+package com.datastructures.may;
+
+/**
+ * @author <a>Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 31/05/22
+ */
+public class BinarySearch {
+	public static int binarysearch(int arr[], int n, int k) {
+		// code here
+		int left = 0;
+		int right = n - 1;
+
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+
+			if (arr[mid] == k) {
+				return mid;
+			} else if (k < arr[mid]) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return -1;
+	}
+}
