402cc8e1427aaad61a7f349c11bc69f5f956f317
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-06-12 16:37:24 +0530

    Recursive

--- src/com/datastructures/june/recursion/Factorial.java
+++ src/com/datastructures/june/recursion/Factorial.java
@@ -1,5 +1,7 @@
 package com.datastructures.june.recursion;
 
+import java.util.Objects;
+
 /**
  * @author <a>Pulkit Aggarwal</a>
  * @version 1.0
@@ -30,12 +32,12 @@ public class Factorial {
 		return fact;
 	}
 
-	private static void fibonacciIterative(int n){
+	private static void fibonacciIterative(int n) {
 		int first = 0;
-		int second  = 1;
+		int second = 1;
 		int c = 0;
 
-		while (c < n){
+		while (c < n) {
 			System.out.println(first + " ");
 
 			int third = first + second;
@@ -45,16 +47,24 @@ public class Factorial {
 		}
 	}
 
-	private static int fibonacciRecursive(int n){
-		if(n <=1){
+	private static int fibonacciRecursive(int n) {
+		if (n <= 1) {
 			return n;
 		}
 
-		return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+		return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+	}
+
+	private static String reverseStringRecursiverly(String s) {
+		if (Objects.equals(s, "")) {
+			return "";
+		}
+		return reverseStringRecursiverly(s.substring(1)) + s.charAt(0);
 	}
 
 	public static void main(String[] args) {
-		System.out.println(findFactorialNumberRecursive(5));
-		System.out.println(findFactorialNumberIterative(4));
+		//System.out.println(findFactorialNumberRecursive(5));
+		//System.out.println(findFactorialNumberIterative(4));
+		System.out.println(reverseStringRecursiverly("pulkit"));
 	}
 }
