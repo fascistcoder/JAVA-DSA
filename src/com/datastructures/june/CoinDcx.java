bddd50499053eca7e79cd6a05411cd71c040f2a9
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-07-22 17:17:29 +0530

    Random

---
+++ src/com/datastructures/june/CoinDcx.java
@@ -0,0 +1,81 @@
+package com.datastructures.june;
+
+/**
+ * @author <a>Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 28/06/22
+ */
+public class CoinDcx {
+	/*
+	Q {3,4,5,7, 2] sum = 7, pair [3,4], [5,2]
+
+	int[] res = new int[];
+
+	int m = 0;
+	int n = 0;
+
+	// Psuedo Code
+	// Brute Force
+	 two for loop
+
+	 // time complexity O(n2)
+
+	 for(int i=0; i<a.length;i++)
+	 {
+	 for(int j=i+1; j<a.length;j++
+	 {
+	 if(a[i]+a[j] == sum){
+	 sout(a[i],a[j]);
+	 }
+	 }
+	 }
+
+
+	 // Optimol Solution
+	 // Sort the array
+
+	 // o(n) hashMap we remove the duplicate
+
+	 // Arrays.stream.distinct O(1)
+
+	 // Time complexity o(nlogn)
+
+	 // This is wrong assumption
+	 [2,3,4,5,7] sum = 6 [2,4]
+
+	 [2,3,3,4,4,5,7] sum = 6
+
+
+	 int i = 0;
+	 int j = a.length - 1;
+
+	 while(){
+	 if(a[i] + a[j] == sum){
+	 sout(a[i] + a[j]);
+	 i ++;
+	 j--;
+	 }
+	 else if(a[i] + a[j] > sum ){
+	 j -- ;}
+	 else(
+	 i++;
+	 }
+
+	 // New Solution
+	 HashMap h = new HaspMap() ;
+	  for( num : arr){
+
+	  if(h.contain(num){
+	  if(num + num  == sum{
+	  sout num, num
+	  }
+	  else{
+	  h.put(num)
+
+	  }
+
+	 */
+
+
+
+}
